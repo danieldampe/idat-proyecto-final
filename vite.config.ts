025b02e257c6def9bb2f06f1f5e58842c98383af
danieldampe <damper.daniel.sm@gmail.com>
2025-10-06 18:38:57 -0500

    adding base

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  // base: 'https://danieldampe.github.io/idat-proyecto-final/'
+  base: 'https://danieldampe.github.io/idat-proyecto-final/'
 })
