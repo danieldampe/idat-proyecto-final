8c95e5a1162b3facb93be8cea4ae577ac6bc0225
danieldampe <damper.daniel.sm@gmail.com>
2025-10-06 16:34:32 -0500

    adding base

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot, type Container } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { Router } from 'wouter'
 
 createRoot(document.getElementById('root') as Container).render(
   <StrictMode>
-    <App />
+    <Router base='https://danieldampe.github.io/idat-proyecto-final'>
+      <App />
+    </Router>
   </StrictMode>
 )
